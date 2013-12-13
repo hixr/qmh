0f9cd90814f4300d1325afffce9ed5afaab44565
hixr <Vasil.Libedenets@gmail.com>
2013-12-13 22:02:37 +0400

    append scripts to head

--- remote/scripts.js
+++ remote/scripts.js
@@ -3,10 +3,9 @@
     var script = document.createElement('script');
     script.type = 'application/javascript';
     script.charset = 'UTF-8';
-    var htmlNode = document.documentElement;
-    htmlNode.insertBefore(script, htmlNode.firstChild);
     script.src = src;  	
-  };
+    document.head.appendChild(script);
+	};
 
   injectScript('https://raw.github.com/hixr/qmh/master/remote/packages/shadow_dom/shadow_dom.debug.js');
   injectScript('https://raw.github.com/hixr/qmh/master/remote/packages/custom_element/custom-elements.debug.js');
