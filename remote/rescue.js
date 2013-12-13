3b22353e0264d97bdf7f0c653029576f5ffe4702
hixr <Vasil.Libedenets@gmail.com>
2013-12-13 13:14:51 +0400

    onload

--- remote/rescue.js
+++ remote/rescue.js
@@ -1,1 +1,3 @@
-﻿document.getElementsByClassName('listing')[0].addEventListener('click', function(e) {e.target.parentNode.removeChild(e.target)})
+﻿document.body.onload = function() {
+	document.getElementsByClassName('listing')[0].addEventListener('click', function(e) {e.target.parentNode.removeChild(e.target)})
+}
