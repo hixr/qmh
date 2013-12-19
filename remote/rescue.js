3ecbcdad8ca2e4cf5aadcada87c406d36ce1ecc3
hixr <Vasil.Libedenets@gmail.com>
2013-12-19 10:39:58 +0400

    locations separated

--- remote/rescue.js
+++ remote/rescue.js
@@ -1,18 +1,40 @@
-document.getElementsByClassName('listing')[0].addEventListener('click', handler);
-function handler(e) {
+if (/index\.php\?c=orders&m=l&st=unassigned&budget=0&fcity=0&ftime=2&q=50/.test(window.location.href)) {
+	processList();
+};
+if (/index\.php\?c=orders&m=order&id=/.test(window.location.href)) {
+	processOrder();
+};
+
+function processList() {
 	
-	var node = e.target;
-	var parent = node.parentNode;
+	console.log('processing list');
 	
-	if (node.tagName != 'A') {
-		return;
-	}; 
+	document.getElementsByClassName('listing')[0].addEventListener('click', handler);
 	
-	while (parent.tagName != 'TBODY') {
-		node = parent;
-		parent = parent.parentNode
+	function handler(e) {
+		
+		var node = e.target;
+		var parent = node.parentNode;
+		
+		if (node.tagName != 'A') {
+			return;
+		}; 
+		
+		while (parent.tagName != 'TBODY') {
+			node = parent;
+			parent = parent.parentNode
+		};
+		
+		parent.removeChild(node);
+	
 	};
 	
-	parent.removeChild(node);
+};
+
+function processOrder() {
+	
+	console.log('processing order');
+	
+	document.getElementById('sms_message').value = 'Спасибо за обращение в ОНЛАЙНТУРС, мы всегда Вам рады. Наш телефон: 8(800)775-33-79 www.onlinetours.ru';
 
 };
--- remote/scripts.js
+++ remote/scripts.js
@@ -1,13 +1,14 @@
 ﻿(function() {
+  
   function injectScript (src) {
     var script = document.createElement('script');
     script.type = 'application/javascript';
     script.charset = 'UTF-8';
     script.src = src;  	
     document.head.appendChild(script);
-	};
+  };
   
-  // Begin inject
+  // Begin injecting
   injectScript('https://raw.github.com/hixr/qmh/master/remote/packages/shadow_dom/shadow_dom.debug.js');
   injectScript('https://raw.github.com/hixr/qmh/master/remote/packages/custom_element/custom-elements.debug.js');
   injectScript('https://raw.github.com/hixr/qmh/master/remote/packages/browser/interop.js');
